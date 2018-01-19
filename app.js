1fb50640fdb58c854ab39b004792618fe9485bcb
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-01-20 00:03:37 +0530

    Added Handlebars template, partials with bootstrap and mongoose

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//Connect to Mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+  useMongoClient: true
+})
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err));
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+
+//INDEX ROUTE
+app.get('/', (req, res) => {
+  const title = 'Welcome'
+  res.render('index', {
+    title: title
+  });
+});
+
+//ABOUT ROUTE
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
 const port = 5000;
 
