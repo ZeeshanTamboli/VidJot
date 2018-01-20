1ef74201cfbf60cb5d841f5934580b3b96f81c2b
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-01-20 14:50:09 +0530

    Mongoose local mongodb

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
 
 //Connect to Mongoose
-mongoose.connect('mongodb://localhost/vidjot-dev', {
-  useMongoClient: true
-})
+mongoose.connect('mongodb://localhost/vidjot-dev')
 .then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err));
+
+//Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
 
 //Handlebars Middleware
 app.engine('handlebars', exphbs({
@@ -17,6 +20,9 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
+//Body parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 //INDEX ROUTE
 app.get('/', (req, res) => {
@@ -29,6 +35,42 @@ app.get('/', (req, res) => {
 //ABOUT ROUTE
 app.get('/about', (req, res) => {
   res.render('about');
+});
+
+//Add Idea Form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
+
+//Process Form
+app.post('/ideas', (req, res) => {
+  let errors = [];
+
+  if(!req.body.title) {
+    errors.push({text: 'Please add a title'});
+  }
+
+  if(!req.body.details) {
+    errors.push({text: 'Please add some details'});
+  }
+
+  if(errors.length > 0) {
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    }
+    new Idea(newUser)
+    .save()
+    .then(idea => {
+      res.redirect('/ideas');
+    })
+  }
 });
 
 const port = 5000;
