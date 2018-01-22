3353990ff95e675162804b39b75982a90ac19892
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-01-22 19:23:38 +0530

    Hooked up a local strategy

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -12,6 +13,9 @@ const app = express();
 //Load routes
 const ideas = require('./routes/ideas');
 const users = require('./routes/users');
+
+//Passport config
+require('./config/passport')(passport);
 
 //Connect to Mongoose
 mongoose.connect('mongodb://localhost/vidjot-dev')
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-//const passport= require('passport');
+const passport= require('passport');
 const router = express.Router();
 
 module.exports = router;
@@ -18,6 +18,15 @@ router.get('/login', (req, res) => {
 //User Register Route
 router.get('/register', (req, res) => {
   res.render('users/register');
+});
+
+//Login Form POST
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/ideas',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })(req, res, next);
 });
 
 //Register from POST
